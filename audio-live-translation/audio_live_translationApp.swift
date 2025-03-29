4f50f8a0bd7ee74330e5405f93ea23388cb97782
Bibin Joseph <bibintomj@gmail.com>
2025-03-29 00:50:44 -0400

    Initial Commit

---
+++ audio-live-translation/audio_live_translationApp.swift
@@ -0,0 +1,17 @@
+//
+//  audio_live_translationApp.swift
+//  audio-live-translation
+//
+//  Created by Bibin Joseph on 2025-03-29.
+//
+
+import SwiftUI
+
+@main
+struct audio_live_translationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
